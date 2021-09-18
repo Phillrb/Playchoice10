3a287ce2b6cf4ae45e3f8becc90e6731f64af53f
Phillip Riscombe-Burton <phillrb@gmail.com>
2021-09-18 16:17:00 +0100

    Adding ATTiny 85 Zapper Mod
    Program an ATTiny 85 over ICSP via Arduino to correct zapper trigger issue with Playchoice 10

---
+++ Zapper_Mod_85/zapper_attiny85/zapper_attiny85.ino
@@ -0,0 +1,28 @@
+
+const int ACTION_PIN = 0;
+const int ZAPPER_FIRE_MIN_MS = 300;
+
+void setup() {
+
+  // Prepare for read
+  pinMode(ACTION_PIN, INPUT);
+
+  // WAIT 1 sec (let inputs settle)
+  delay(1000);
+}
+
+void loop() {
+
+  // Firing?
+  if(digitalRead(ACTION_PIN) == LOW) {
+
+    // Hold the firing signal for longer!
+    pinMode(ACTION_PIN, OUTPUT);
+    digitalWrite(ACTION_PIN, LOW);
+    delay(ZAPPER_FIRE_MIN_MS);
+
+    // Stop extending the fire signal
+    pinMode(ACTION_PIN, INPUT);
+  }
+  
+}
